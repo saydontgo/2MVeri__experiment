0ebd140cc5b2d9aa1af88ae116e357e437710268
zjf <meetsviva@163.com>
2025-02-27 20:35:04 +0800

    合并共用代码

--- topo/FatTree6/receive.py
+++ topo/FatTree6/receive.py
@@ -6,53 +6,12 @@ import json
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 
 
-from scapy.all import sniff,  get_if_list
-from scapy.all import Packet, IPOption
-from scapy.all import PacketListField, ShortField,BitField, FieldLenField
+from scapy.all import sniff
+from scapy.all import Packet
 from scapy.all import IP, UDP, TCP
-from scapy.layers.inet import _IPOption_HDR
-
+from tools import get_if, SwitchTrace, IPOption_TAG, IPOption_MRI
 from topo import Topology
 
-def get_if():
-    ifs=get_if_list()
-    iface=None # "h1-eth0"
-    print(ifs)
-    for i in get_if_list():
-        if "eth0" in i:
-            iface=i
-            break
-    if not iface:
-        print("Cannot find interface eth1")
-        exit(1)
-    return iface
-
-class SwitchTrace(Packet):
-    fields_desc = [ BitField("swid", 0x0, 14),
-                  BitField("inport", 0x0, 9),
-                  BitField("outport", 0x0, 9)]#qdepth
-
-    def extract_padding(self, p):
-                return "", p
-
-class IPOption_TAG(IPOption):
-    name = "TAG"
-    option = 1
-    fields_desc = [ _IPOption_HDR,
-                    BitField("length", 0, 8),
-                    # BitField("bloom_filter", 0, 16),
-                    BitField("prime_product", 1, 32)]
-
-class IPOption_MRI(IPOption):
-    name = "MRI"
-    option = 2
-    fields_desc = [ _IPOption_HDR,
-                    FieldLenField("length", None, fmt="B", length_of="swtraces",  adjust=lambda pkt, l:l*2+4),
-                    BitField("count", 0, 16),
-                    PacketListField("swtraces",
-                                   [],
-                                   SwitchTrace,
-                                   count_from=lambda pkt:(pkt.count*1)) ]
     
 def load_rules(rule_path):
     rules = {}
--- topo/FatTree6/test.py
+++
@@ -1,6 +0,0 @@
-import re
-
-str=['10.0.1.2/24', '10.0.1.13/24']
-
-for s in str:
-    print(re.findall('(.*)/', s))
